2944263e4c607b0c80888f5cd7ef4c16b7422bb5
JC <johncedricktee@gmail.com>
2018-01-12 21:13:02 -0800

    added comments

--- Student.php
+++ Student.php
@@ -12,7 +12,7 @@
  * @author JC
  */
 class Student {
-    //put your code here
+    //constructor
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -20,14 +20,17 @@ class Student {
         $this->grades = array();
     }
     
+    //adds email of student
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    //adds a grade of the student
     function add_grade($grade) {
        $this->grades[] = $grade;
     }
     
+    //calculates the average of the grades of a student
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -35,6 +38,7 @@ class Student {
         return $total / count($this->grades);
     }
     
+    //returns a string of student information
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
